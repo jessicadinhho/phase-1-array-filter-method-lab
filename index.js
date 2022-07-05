cff7416c2c227826fb8bda0996e9838c823de154
Jessica Ho <jessicadinhho@gmail.com>
2022-07-04 23:37:32 -0700

    commit tree 6da09b269f8c4137ef116ff3834aee4f3fc0901f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr, str) {
+    let match = arr.filter(Element => Element.toLowerCase() === str.toLowerCase());
+    return match
+}
+
+function fuzzyMatch(arr, str) {
+    let match = arr.filter(element => element[0] === str[0])
+    return match
+}
+
+function matchName(arr, str) {
+    let match = arr.filter(Element => Element.name === str)
+    return match
+}
